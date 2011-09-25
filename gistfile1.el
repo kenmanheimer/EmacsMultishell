b2b6daeb919cff0bcd119855933e3284ade03cf6
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-09-25 16:28:07 +0000

    How I does it

--- gistfile1.el
+++ gistfile1.el
@@ -1,5 +1,49 @@
-(defun git-grep (regexp)
-  (interactive "sSearch in git repo: ")
-  (grep (format "GIT_PAGER='' git grep -nH --no-color -i \"%s\" -- $(git rev-parse --show-toplevel)" regexp)))
+;; There's something similar (but fancier) in vc-git.el: vc-git-grep
 
-(global-set-key (kbd "C-x ?") 'git-grep)
+;; -I means don't search through binary files
+
+;; --no-color, oddly enough, is required to allow emacs to colorize the output
+
+(defcustom git-grep-switches "--extended-regexp -I -n --ignore-case --no-color"
+  "Switches to pass to `git grep'."
+  :type 'string)
+
+(defcustom git-grep-default-work-tree (expand-file-name "~/work/adtrack")
+  "Top of your favorite git working tree.  \\[git-grep] will search from here if it cannot figure out where else to look."
+  :type 'directory
+  )
+
+(when (require 'vc-git nil t)
+
+  ;; Uncomment this to try out the built-in-to-Emacs function.
+  ;;(defalias 'git-grep 'vc-git-grep)
+
+  (defun git-grep (command-args)
+    (interactive
+     (let ((root (vc-git-root default-directory)))
+       (when (not root)
+         (setq root git-grep-default-work-tree)
+         (message "git-grep: %s doesn't look like a git working tree; searching from %s instead" default-directory root))
+       (list (read-shell-command "Run git-grep (like this): "
+                                 (format (concat
+                                          "cd %s && "
+                                          "git grep %s -e %s")
+                                         root
+                                         git-grep-switches
+                                         (let ((thing (and
+
+                                        ; don't snarf stuff from the
+                                        ; buffer if we're not looking
+                                        ; at a file.  Perhaps we
+                                        ; should also check to see if
+                                        ; the file is part of a git
+                                        ; repo.
+                                                       buffer-file-name
+                                                       (thing-at-point 'symbol))))
+                                           (or (and thing (progn
+                                                            (set-text-properties 0 (length thing) nil thing)
+                                                            (shell-quote-argument (regexp-quote thing))))
+                                               "")))
+                                 'git-grep-history))))
+    (let ((grep-use-null-device nil))
+      (grep command-args))))
