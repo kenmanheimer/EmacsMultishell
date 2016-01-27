8cea630b74fe4b8806d250d551be4a23d346fa59
Ken Manheimer <ken.manheimer@gmail.com>
2016-01-27 03:13:00 -0500

    multishell - don't inhibit buffer names from completions.
    Allow duplicates as sign of buffers that currently exist vs historical
    entries.

--- multishell.el
+++ multishell.el
@@ -497,7 +497,9 @@ Return the supplied name, if provided, else return nil."
                                   (with-current-buffer buffer
                                     ;; Shell mode buffers.
                                     (derived-mode-p 'shell-mode))
-                                  (not (multishell-history-entries name))
+                                  ;; Allow duplicates, as sign of buffers that
+                                  ;; currently exist vs historical entries.
+                                  ;;(not (multishell-history-entries name))
                                   name)))
                          (buffer-list)))
            multishell-history))
